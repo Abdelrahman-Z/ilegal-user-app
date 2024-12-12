b1d07ff8bb611faf77da9ec6672d3ab14a79463e
Abdelrahman Zidan <abdelrahmanziedan@gmail.com>
2024-12-12 14:33:25 +0200

    fix build error

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,21 +2,22 @@ import { getMessages } from "next-intl/server";
 import "../globals.css";
 import Providers from "@/components/providers";
 
-
 export default async function RootLayout({
   children,
-  params: { locale },
-}: Readonly<{
+}: {
   children: React.ReactNode;
-  params: { locale: string };
-}>) {
+}) {
+  // Use headers to detect locale
+  const headers = new Headers();
+  const acceptLanguage = headers.get("accept-language") || "en";
+  const locale = acceptLanguage.split(",")[0] || "en"; // Default to English if not specified
+
+  // Fetch translations for the detected locale
   const messages = await getMessages();
 
   return (
     <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
-      <body
-        className={`antialiased`}
-      >
+      <body className="antialiased">
         <Providers locale={locale} messages={messages}>
           {children}
         </Providers>
